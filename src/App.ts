4c0c7057f174923e3d8d3ac0fa95d07e88b46848
Aravind <s.aravind.sankar@gmail.com>
2024-10-02 01:50:47 +0530

    Swagger Changes

--- src/App.ts
+++ src/App.ts
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 import OrganisationRoutes from "./routes/OrganisationRoutes";
 import BoardRoutes from "./routes/BoardRoutes";
 import ProductRoutes from "./routes/ProductRoutes";
-import { setupMorganMiddleware } from "./middlewares/MorganLoggerMiddleware"; // Import Morgan setup function
-import { responseBodyCapture } from "./middlewares/ResponseBodyCaptureMiddleware"; // Import response body capture middleware
-import { jwtAuthorization } from "./middlewares/JwtAuthorizationMiddleware"; // Import JWT authorization middleware
-import ErrorMiddleware from "./middlewares/ErrorMiddleware"; // Import JWT authorization middleware
+import { setupMorganMiddleware } from "./middlewares/MorganLoggerMiddleware";
+import { responseBodyCapture } from "./middlewares/ResponseBodyCaptureMiddleware";
+import { jwtAuthorization } from "./middlewares/JwtAuthorizationMiddleware";
+import ErrorMiddleware from "./middlewares/ErrorMiddleware";
 import { limiter } from "./middlewares/RateLimitMiddleware";
 import { compressionMiddleware } from "./middlewares/CompressionMiddleware";
+import { setupSwagger } from "./config/SwaggerConfig";
 
 dotenv.config();
 
@@ -21,9 +22,14 @@ app.use(responseBodyCapture);
 
 // Setup Morgan middleware
 setupMorganMiddleware(app);
+
 app.use(limiter);
+
 // Apply compression middleware globally
 app.use(compressionMiddleware);
+
+// Setup Swagger
+setupSwagger(app);
 
 // Use JWT authorization middleware for protected routes
 app.use("/api", jwtAuthorization, OrganisationRoutes);
---
+++ src/config/SwaggerConfig.ts
@@ -0,0 +1,35 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Quotatation Service API",
+      version: "1.0.0",
+      description: "Chit Plus Quotation Service API",
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts", "./src/controllers/*.ts"], // Path to the API docs
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
